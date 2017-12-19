90867d6ef22e70fd8c7feee04eed01d4a80aa41e
GillinedUp <myrreah@gmail.com>
2017-12-19 18:51:54 +0200

    created Populator class; created tests; minor fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
-//        System.out.println(solution.returnAllRelationshipsForBand("Judas Priest"));
+        Populator populator = new Populator(solution);
+        populator.createNodes();
+        populator.createRelationships();
+
         solution.databaseStatistics();
-//        solution.databaseSchema();
+        solution.databaseSchema();
+
+        System.out.println(solution.returnAllRelationshipsForPerson("Rob Halford"));
+        System.out.println(solution.returnAllRelationshipsForBand("Judas Priest"));
+        System.out.println(solution.returnAllRelationshipsForGenre("Heavy metal"));
+        System.out.println(solution.returnAllRelationshipsForCountry("UK"));
+
+        System.out.println(solution.returnPathBetweenBands("Judas Priest", "Gloryhammer"));
+        System.out.println(solution.returnPathBetweenGenres("Thrash metal", "Death metal"));
+        System.out.println(solution.returnPathBetweenCountries("Germany", "UK"));
+        System.out.println(solution.returnPathBetweenPersons("Hansi Kürsch", "André Olbrich"));
+        System.out.println(solution.returnPathBetweenBandAndCountry("Tank", "Switzerland"));
+        System.out.println(solution.returnPathBetweenBandAndGenre("Gloryhammer", "Speed metal"));
+        System.out.println(solution.returnPathBetweenPersonAndBand("Ian Hill", "Diamond Head"));
+        System.out.println(solution.returnPathBetweenPersonAndCountry("Chuck Schuldiner", "US"));
+        System.out.println(solution.returnPathBetweenPersonAndGenre("Ted Aguilar", "Death metal"));
+        System.out.println(solution.returnPathBetweenGenreAndCountry("Doom metal", "Sweden"));
     }
 
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -39,7 +39,11 @@ public class Solution {
 
     public String createPerson(final String personName, final int yearOfBirth) {
         return graphDatabase.runCypher("CREATE (n:Person {name: \"" + personName + "\", " +
-                "year: {" + yearOfBirth + "})");
+                "year: " + yearOfBirth + "})");
+    }
+
+    public String createPerson(final String personName) {
+        return graphDatabase.runCypher("CREATE (n:Person {name: \"" + personName + "\"})");
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////
@@ -63,7 +67,7 @@ public class Solution {
     {
         return graphDatabase.runCypher("MATCH (p:Person {name:\"" + personName + "\"}), " +
                 "(b:Band {name: \"" + bandName + "\"})\n" +
-                "CREATE (p)-[:IS_MEMBER_OF {year_of_join: {" + yearOfJoin + "}, " +
+                "CREATE (p)-[:IS_MEMBER_OF {year_of_join: " + yearOfJoin + ", " +
                 "role: \"" + role + "\"}]->(b)");
     }
 
@@ -96,9 +100,9 @@ public class Solution {
                                          final String nodeName1,
                                          final String nodeName2)
     {
-        return graphDatabase.runCypher("MATCH (n1:" + nodeType1 + " { name: \"" + nodeName1 + "\" })," +
-                "(n2:" + nodeType2 + " { name: \"" + nodeName2 +"\" }), " +
-                "p = shortestPath((n1)--(n2))\n" +
+        return graphDatabase.runCypher("MATCH (n1:" + nodeType1 + " {name: \"" + nodeName1 + "\"})," +
+                "(n2:" + nodeType2 + " {name: \"" + nodeName2 +"\"}), " +
+                "p = shortestPath((n1)-[*]-(n2))\n" +
                 "RETURN p");
     }
 
@@ -115,7 +119,7 @@ public class Solution {
     }
 
     public String returnPathBetweenPersons(final String personName1, final String personName2) {
-        return returnPathBetweenNodes("Person", "Band", personName1, personName2);
+        return returnPathBetweenNodes("Person", "Person", personName1, personName2);
     }
 
     public String returnPathBetweenBandAndCountry(final String bandName, final String countryName) {
@@ -141,149 +145,5 @@ public class Solution {
     public String returnPathBetweenGenreAndCountry(final String genreName, final String countryName) {
         return returnPathBetweenNodes("Genre", "Country", genreName, countryName);
     }
-
-    public void createNodes() {
-        System.out.println(createBand("Gloryhammer", 2010));
-        System.out.println(createBand("Tank", 1980));
-        System.out.println(createBand("Judas Priest", 1970));
-        System.out.println(createBand("Death Angel", 1982));
-        System.out.println(createBand("Death", 1984));
-        System.out.println(createBand("Candlemass", 1984));
-        System.out.println(createBand("Anthrax", 1981));
-        System.out.println(createBand("Blind Guardian", 1986));
-        System.out.println(createBand("Gamma Ray", 1989));
-        System.out.println(createBand("Diamond Head", 1976));
-        System.out.println(createCountry("UK", "Europe"));
-        System.out.println(createCountry("US", "North America"));
-        System.out.println(createCountry("Switzerland", "Europe"));
-        System.out.println(createCountry("Sweden", "Europe"));
-        System.out.println(createCountry("Germany", "Europe"));
-        System.out.println(createGenre("Heavy metal", "clean", "mid", "mild"));
-        System.out.println(createGenre("Thrash metal", "harsh", "fast", "heavy"));
-        System.out.println(createGenre("Death metal", "growl", "fast", "heavy"));
-        System.out.println(createGenre("Doom metal", "clean", "slow", "heavy"));
-        System.out.println(createGenre("Power metal", "clean", "fast", "mild"));
-        System.out.println(createGenre("Speed metal", "clean", "fast", "mild"));
-        System.out.println(createPerson("James Cartwright", 1986));
-        System.out.println(createPerson("Ben Turk", -1));
-        System.out.println(createPerson("Paul Templing", -1));
-        System.out.println(createPerson("Christopher Bowes", 1986));
-        System.out.println(createPerson("Thomas Winkler", 1985));
-        System.out.println(createPerson("Algy Ward", 1959));
-        System.out.println(createPerson("Mick Tucker", -1));
-        System.out.println(createPerson("Cliff Evans", 1962));
-        System.out.println(createPerson("Bruce Bisland", 1958));
-        System.out.println(createPerson("Ian Hill", 1951));
-        System.out.println(createPerson("Rob Halford", 1951));
-        System.out.println(createPerson("Glenn Tipton", 1947));
-        System.out.println(createPerson("Scott Travis", 1961));
-        System.out.println(createPerson("Rob Cavestany", 1968));
-        System.out.println(createPerson("Mark Osegueda", 1969));
-        System.out.println(createPerson("Ted Aguilar", -1));
-        System.out.println(createPerson("Damien Sisson", 1981));
-        System.out.println(createPerson("Will Carroll", 1973));
-        System.out.println(createPerson("Chuck Schuldiner", 1967));
-        System.out.println(createPerson("Scott Clendenin", 1968));
-        System.out.println(createPerson("Richard Christy", 1974));
-        System.out.println(createPerson("Shannon Hamm", 1967));
-        System.out.println(createPerson("Leif Edling", 1963));
-        System.out.println(createPerson("Mats Björkman", -1));
-        System.out.println(createPerson("Jan Lindh", -1));
-        System.out.println(createPerson("Lars Johansson", -1));
-        System.out.println(createPerson("Mats Levén", 1964));
-        System.out.println(createPerson("Scott Ian", 1963));
-        System.out.println(createPerson("Charlie Benante", 1962));
-        System.out.println(createPerson("Frank Bello", 1965));
-        System.out.println(createPerson("Joey Belladonna", 1960));
-        System.out.println(createPerson("Jonathan Donais", 1979));
-        System.out.println(createPerson("Hansi Kürsch", 1966));
-        System.out.println(createPerson("André Olbrich", 1967));
-        System.out.println(createPerson("Marcus Siepen", 1968));
-        System.out.println(createPerson("Frederik Ehmke", 1978));
-        System.out.println(createPerson("Kai Hansen", 1963));
-        System.out.println(createPerson("Dirk Schlächter", 1965));
-        System.out.println(createPerson("Henjo Richter", 1963));
-        System.out.println(createPerson("Michael Ehré", 1971));
-        System.out.println(createPerson("Frank Beck", 1969));
-        System.out.println(createPerson("Brian Tatler", 1963));
-        System.out.println(createPerson("Karl Wilcox", -1));
-        System.out.println(createPerson("Andy Abberley", -1));
-        System.out.println(createPerson("Rasmus Bom Andersen", 1984));
-        System.out.println(createPerson("Dean Ashton", -1));
-    }
-
-    public void createRelationships() {
-        System.out.println(createOriginRelationship("Judas Priest", "UK"));
-        System.out.println(createOriginRelationship("Gloryhammer", "UK"));
-        System.out.println(createOriginRelationship("Gloryhammer", "Switzerland"));
-        System.out.println(createOriginRelationship("Tank", "UK"));
-        System.out.println(createOriginRelationship("Death Angel", "US"));
-        System.out.println(createOriginRelationship("Candlemass", "Sweden"));
-        System.out.println(createOriginRelationship("Death", "US"));
-        System.out.println(createOriginRelationship("Anthrax", "US"));
-        System.out.println(createOriginRelationship("Diamond Head", "UK"));
-        System.out.println(createOriginRelationship("Blind Guardian", "Germany"));
-        System.out.println(createOriginRelationship("Gamma Ray", "Germany"));
-        System.out.println(createPlaysRelationship("Judas Priest", "Heavy metal"));
-        System.out.println(createPlaysRelationship("Gloryhammer", "Power metal"));
-        System.out.println(createPlaysRelationship("Tank", "Heavy metal"));
-        System.out.println(createPlaysRelationship("Tank", "Speed metal"));
-        System.out.println(createPlaysRelationship("Death Angel", "Thrash metal"));
-        System.out.println(createPlaysRelationship("Candlemass", "Doom metal"));
-        System.out.println(createPlaysRelationship("Death", "Death metal"));
-        System.out.println(createPlaysRelationship("Anthrax", "Thrash metal"));
-        System.out.println(createPlaysRelationship("Diamond Head", "Heavy metal"));
-        System.out.println(createPlaysRelationship("Blind Guardian", "Power metal"));
-        System.out.println(createPlaysRelationship("Blind Guardian", "Speed metal"));
-        System.out.println(createPlaysRelationship("Gamma Ray", "Power metal"));
-        System.out.println(createIsMemberOfRelationship("James Cartwright", "Gloryhammer", 2010, "bass"));
-        System.out.println(createIsMemberOfRelationship("Ben Turk", "Gloryhammer", 2010, "drums"));
-        System.out.println(createIsMemberOfRelationship("Paul Templing", "Gloryhammer", 2010, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Christopher Bowes", "Gloryhammer", 2010, "keyboards"));
-        System.out.println(createIsMemberOfRelationship("Thomas Winkler", "Gloryhammer", 2010, "vocals"));
-        System.out.println(createIsMemberOfRelationship("Algy Ward", "Tank", 1980, "vocals, bass"));
-        System.out.println(createIsMemberOfRelationship("Mick Tucker", "Tank", 1983, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Cliff Evans", "Tank", 1984, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Bruce Bisland", "Tank", 2001, "drums"));
-        System.out.println(createIsMemberOfRelationship("Ian Hill", "Judas Priest", 1970, "bass"));
-        System.out.println(createIsMemberOfRelationship("Rob Halford", "Judas Priest", 1973, "vocals"));
-        System.out.println(createIsMemberOfRelationship("Glenn Tipton", "Judas Priest", 1974, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Scott Travis", "Judas Priest", 1989, "drums"));
-        System.out.println(createIsMemberOfRelationship("Richie Faulkner", "Judas Priest", 2010, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Rob Cavestany", "Death Angel", 1982, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Mark Osequeda", "Death Angel", 1984, "vocals"));
-        System.out.println(createIsMemberOfRelationship("Ted Aguilar", "Death Angel", 2001, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Damien Sisson", "Death Angel", 2009, "bass"));
-        System.out.println(createIsMemberOfRelationship("Will Carroll", "Death Angel", 2009, "drums"));
-        System.out.println(createIsMemberOfRelationship("Chuck Schuldiner", "Death", 1984, "guitars, vocals"));
-        System.out.println(createIsMemberOfRelationship("Scott Clendenin", "Death", 1996, "bass"));
-        System.out.println(createIsMemberOfRelationship("Richard Christy", "Death", 1996, "drums"));
-        System.out.println(createIsMemberOfRelationship("Shannon Hamm", "Death", 1996, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Lief Edling", "Candlemass", 1984, "bass"));
-        System.out.println(createIsMemberOfRelationship("Mats Björkman", "Candlemass", 1985, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Jan Lindh", "Candlemass", 1987, "drums"));
-        System.out.println(createIsMemberOfRelationship("Mats Levén", "Candlemass", 2006, "vocals"));
-        System.out.println(createIsMemberOfRelationship("Scott Ian", "Anthrax", 1981, "guitars, vocals"));
-        System.out.println(createIsMemberOfRelationship("Charlie Benante", "Anthrax", 1983, "drums"));
-        System.out.println(createIsMemberOfRelationship("Frank Bello", "Anthrax", 1984, "bass"));
-        System.out.println(createIsMemberOfRelationship("Joey Belladonna", "Anthrax", 1984, "vocals"));
-        System.out.println(createIsMemberOfRelationship("Jonathan Donais", "Anthrax", 2013, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Hansi Kürsch", "Blind Guardian", 1986, "vocals"));
-        System.out.println(createIsMemberOfRelationship("André Olbrich", "Blind Guardian", 1986, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Marcus Siepen", "Blind Guardian", 1987, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Frederik Ehmke", "Blind Guardian", 2005, "drums"));
-        System.out.println(createIsMemberOfRelationship("Kai Hansen", "Gamma Ray", 1989, "guitars, vocals"));
-        System.out.println(createIsMemberOfRelationship("Dirk Schlächter", "Gamma Ray", 1990, "guitars, keyboards, bass"));
-        System.out.println(createIsMemberOfRelationship("Henjo Richter", "Gamma Ray", 1989, "guitars, keyboards"));
-        System.out.println(createIsMemberOfRelationship("Michael Ehré", "Gamma Ray", 1989, "drums"));
-        System.out.println(createIsMemberOfRelationship("Frank Beck", "Gamma Ray", 1989, "vocals"));
-        System.out.println(createIsMemberOfRelationship("Brian Tatler", "Diamond Head", 1976, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Karl Wilcox", "Diamond Head", 1992, "drums"));
-        System.out.println(createIsMemberOfRelationship("Andy Abberley", "Diamond Head", 2006, "guitars"));
-        System.out.println(createIsMemberOfRelationship("Rasmus Bom Andersen", "Diamond Head", 2014, "vocals"));
-        System.out.println(createIsMemberOfRelationship("Dean Ashton", "Diamond Head", 2016, "bass"));
-    }
-
-
 
 }
